8bd207ea0efff5c0de4b9bab2470fab21b643264
souvick <souvickrocks95@gmail.com>
2019-07-28 10:57:59 +0530

    souvick

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { LOCAL_STORAGE, StorageService } from 'angular-webstorage-service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+ 
+@Injectable()
 export class AppComponent {
-  title = 'lms1';
+
+  constructor(@Inject(LOCAL_STORAGE) private storage: StorageService) { }
+
+  id = 'user';
+
+  person = {
+    name: "Obaseki Nosa",
+    location: "Lagos",
+};
+
+  public addStudent(): void {
+    const awesomenessLevel: string = this.id;
+    console.log(awesomenessLevel);
+    this.storage.set(awesomenessLevel,JSON.stringify(this.person));
 }
+
+  printStuden() : void {
+    console.log(this.storage.get('user'));
+  }
+
+  deleteStuden() : void {
+    window.localStorage.removeItem('user');
+  }
+
+};
+
+
