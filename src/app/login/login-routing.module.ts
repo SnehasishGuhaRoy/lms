a00e17b2e9d186a499918a9a5ab97e9fda8a772f
souvick <souvickrocks95@gmail.com>
2019-08-18 12:25:15 +0530

    souvick

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
 
 const routes: Routes = [
     {
         path: '',
         component: LoginComponent
+    },
+    {
+        path: 'signUp',
+        component: SignUpComponent
     }
 ];
 
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,20 +1,22 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatButtonModule, MatCheckboxModule, MatInputModule } from '@angular/material';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../shared/library/material.module';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import { SignUpComponent } from "/home/souvick/lms/src/app/login/sign-up/sign-up.component";
 
 @NgModule({
     imports: [
         CommonModule,
         LoginRoutingModule,
-        MatInputModule,
-        MatCheckboxModule,
-        MatButtonModule,
+        MaterialModule,
+        FormsModule,
+        ReactiveFormsModule,
         FlexLayoutModule.withConfig({addFlexToParent: false})
     ],
-    declarations: [LoginComponent]
+    declarations: [SignUpComponent,
+        LoginComponent]
 })
 export class LoginModule {}
